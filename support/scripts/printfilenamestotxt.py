3b48de8758ee7b5bd2e244be9be2d8ccce09576f
Miles Peyton <milespeyton@gmail.com>
2014-08-11 13:21:19 -0400

    Add script to print all filenames.

---
+++ support/scripts/printfilenamestotxt.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+rootdir = '../..'
+
+validFiletype = ['html','png']
+outputname = 'allfilenames.txt'
+open(outputname, 'w').close()
+with open( outputname, 'a' ) as txtFile:
+    for root, subFolders, files in os.walk(rootdir):
+            for filename in files:
+                for type in validFiletype:
+                    if type in filename:
+                        txtFile.write(filename + '\n')
+                        print(filename + '\n')
+                        break
